ebc0201cab31847dfef8fb6dd1de79d6871a75a2
TSharma05 <tressa.sharma@gmail.com>
2022-07-17 08:35:25 -0500

    completed the final function to determine if the material is an organic peroxide or not. Updated the README with the process and final answers

--- README.md
+++ README.md
@@ -14,3 +14,12 @@
 - Loop through the products dictionaries and assign variables for the peroxy groups, max concentration, and molecular mass. This is for ease of calculations
 - Multiple the number of peroxy groups by the max concentration then divide by the molecular mass
 - This will be completed for each chemical in the product and added to a total sum. That sum is then multiplied by 16, this is the available oxygen content for that product
+
+**Step 4: Create function to determine the classification of the material**
+- I can now use the functions from step 2 and 3 to determine the classification.
+- The product is passed to the function and then variables for the hydrogen peroxide and available oxygen content are created and they call the functions with the product being passed as a parameter.
+- The H<sub>2</sub>O<sub>2</sub> % and O<sub>a</sub> are then compared using CFR 173.128(a)(4).
+- Once the function is ran the output will inform us if the products are organic peroxides or not.
+
+**Which products are or are not organic peroxides?**
+- According to CFR 173.128(a)(4) both product 2 and 3 are organic peroxides, while product 1 is not an organic peroxide according to CFR 173.128(a)(4)(i)
--- chem_calc.py
+++ chem_calc.py
@@ -8,12 +8,14 @@ def get_H2O2(param):
     list_of_values = [ele[key] for ele in param]
 
     if value in list_of_values:
-        print(f"{value} is in the list")
+        # print(f"{value} is in the list")
         sum_max = sum(ele['max (%)'] for ele in param)
         percent_H2O2 = round((param[list_of_values.index(value)]['max (%)'] / sum_max) * 100, 2)
-        print(percent_H2O2)
+        # print(percent_H2O2)
+        return percent_H2O2
     else:
-        print(f"{value} is not in the list")
+        # print(f"{value} is not in the list")
+        return 0
 
 
 # get_H2O2(product_1)
@@ -32,10 +34,30 @@ def get_avail_oxygen(param):
         m_i = i['molecular mass']
         calc = (n_i * c_i) / m_i
         sum_of += calc
-    print(sum_of)
+    # print(sum_of)
     O_a = round((16 * sum_of), 2)
-    print(f"{O_a}% of oxygen is available")
+    # print(f"{O_a}% of oxygen is available")
+    return O_a
 
 # get_avail_oxygen(product_1)
 # get_avail_oxygen(product_2)
 # get_avail_oxygen(product_3)
+
+
+def determine_classification(param):
+    percent_H2O2 = get_H2O2(param)
+    available_O_a = get_avail_oxygen(param)
+
+    if percent_H2O2 <= 1.0 and available_O_a <= 1.0:
+        return "not an Organic Peroxide according to CFR 173.128(a)(4)(i)"
+    elif percent_H2O2 > 1.0 and percent_H2O2 <= 7.0 and available_O_a <= 0.5:
+        return "not an Organic Peroxide according to CFR 173.128(a)(4)(ii)"
+    else:
+        return "an Organic Peroxide according to CFR 173.128(a)(4)"
+
+
+print(f"Product 1 is {determine_classification(product_1)}")
+print(f"Product 2 is {determine_classification(product_2)}")
+print(f"Product 3 is {determine_classification(product_3)}")
+# determine_classification(product_2)
+# determine_classification(product_3)
